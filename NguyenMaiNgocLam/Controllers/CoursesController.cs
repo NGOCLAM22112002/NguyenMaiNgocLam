a336d553221d5e7f37c592989a73f6f11be6606b
Admin <Admin@MSI>
2023-03-11 16:42:18 +0700

    Add project files.

---
+++ NguyenMaiNgocLam/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using NguyenMaiNgocLam.Models;
+using NguyenMaiNgocLam.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenMaiNgocLam.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                categories = _dbContext.categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
